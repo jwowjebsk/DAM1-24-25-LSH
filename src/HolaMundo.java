c5979b08da6fbf41006e2857e92951738256df63
jwowjebsk <ayshagonzalezvillanueva@gmail.com>
2025-05-14 13:44:59 +0200

    test 6

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) throws Exception {
         System.out.println("Ola Mundo!! LSH");
-        System.out.println("Test 5");
+        System.out.println("Test 6");
     }
 }
